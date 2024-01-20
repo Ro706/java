49575c7bdc954ee1bd58562a871b87502be4706f
Ro706 <rohitmandal99091@gmail.com>
2024-01-20 16:35:01 +0530

    add

---
+++ College.java
@@ -0,0 +1,6 @@
+public class College{
+	public static void main(String[] args){
+	 	String name = "Symbiosis institute of technology";
+		System.out.println("My college name is "+name+"Nagpur.\n "+name+"is Located in Nagpur.\n");
+	}
+}
---
+++ Sample.java
@@ -0,0 +1,11 @@
+public class Sample{
+	public static void main(String[] args){
+		int a,b,c,d,e;
+		a =10;
+		b = 12;
+		c = 14;
+		d = 16;
+		e = 18;
+		System.out.println("a = "+a+"\nb = "+b+"\nc = "+c+"\nd = "+d+"\ne = "+e);
+	}
+}	
