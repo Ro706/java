97b1f46ed592d2507a111df2790f399141a4b6e4
Ro706 <rohitmandal99091@gmail.com>
2024-01-24 00:23:13 +0530

    add file

---
+++ if-else/OddEven.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class OddEven{
+	public static void main(String[] args){
+		Scanner myObj = new Scanner(System.in);
+		System.out.println("Enter num:");
+		int num = myObj.nextInt();
+		if (num%2 == 0)
+		        System.out.println("Number is Even");
+		else
+			System.out.println("Number is Odd");
+	}
+}	
