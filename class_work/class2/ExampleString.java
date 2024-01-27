ad53ca82e566b3f2809a58ff631005bbbeb26db7
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 16:43:22 +0530

    add file

--- class_work/class2/ExampleString.java
+++ class_work/class2/ExampleString.java
@@ -4,5 +4,12 @@ public class ExampleString{
 		System.out.println("city name is "+city);
 		int citylen = city.length();
 		System.out.println("Length of city is "+citylen);
+		int index = city.indexOf("o");
+		System.out.println("index of k is : "+index);
+		int lindex = city.lastIndexOf("o");
+                System.out.println("Last index of o is : "+lindex);
+		char ch1 = city.charAt(4);
+		System.out.println("character at index 6 is index 6 is "+ch1);
+
 	}
 }
