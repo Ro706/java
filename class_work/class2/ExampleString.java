41c08fb63641167f8692767c711ac122a711b147
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 16:27:30 +0530

    add file

---
+++ class_work/class2/ExampleString.java
@@ -0,0 +1,8 @@
+public class ExampleString{
+	public static void main(String[] args){
+		String city = "bokaro";
+		System.out.println("city name is "+city);
+		int citylen = city.length();
+		System.out.println("Length of city is "+citylen);
+	}
+}
