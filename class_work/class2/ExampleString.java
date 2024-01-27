44be144e171c64fb8e0f6ea2cbe99ae5da2649ae
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 16:58:29 +0530

    add file

--- class_work/class2/ExampleString.java
+++ class_work/class2/ExampleString.java
@@ -1,6 +1,6 @@
 public class ExampleString{
 	public static void main(String[] args){
-		String city = "bokaro";
+		String city = "amravat";
 		System.out.println("city name is "+city);
 		int citylen = city.length();
 		System.out.println("Length of city is "+citylen);
@@ -10,6 +10,14 @@ public class ExampleString{
                 System.out.println("Last index of o is : "+lindex);
 		char ch1 = city.charAt(4);
 		System.out.println("character at index 6 is index 6 is "+ch1);
+		int n = citylen-1,flag = 0;
+		while (n != -1){
+			if (city.charAt(n)=='a'){
+				flag += 1;
+			}
+			n--;
+		}
+		System.out.println("The number of time 'a' comming in given string is "+flag);
 
 	}
 }
