6c1f6d95ba7363f36f0f6d2ae30fb5acb129c97f
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 16:12:40 +0530

    add file

---
+++ class_work/class2/ExampleBoolean.java
@@ -0,0 +1,8 @@
+public class ExampleBoolean{
+	public static void main(String[] args){
+		boolean a = true;
+		System.out.println("a = "+a);
+		boolean b = !a;
+		System.out.println("b = "+b);
+	}
+}
