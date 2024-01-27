14aa9fcc0d25bfad7be36e4ccea2fddd00e50ef0
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 14:37:17 +0530

    add file

--- class_work/class2/ExampleByte.java
+++ class_work/class2/ExampleByte.java
@@ -1,6 +1,6 @@
 public class  ExampleByte{
 	public static void main(String[] args){
-		byte a = 13;
+		byte a = -129;
 		System.out.println("a="+a);
 	
 	}
---
+++ class_work/class2/Exampleshort.java
@@ -0,0 +1,6 @@
+public class Exampleshort{
+	public static void main(String[] args){
+		short a = -32769;
+		System.out.println("a ="+a);
+	}
+}
