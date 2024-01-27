815bbbab975efe25e966506820a8932be7839624
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 15:39:06 +0530

    add file

---
+++ class_work/class2/ExampleDouble.java
@@ -0,0 +1,8 @@
+public class ExampleDouble{
+	public static void main(String[] args){
+		double a = 500000.45;
+		double b = -500000.45;
+		System.out.println("a ="+a);
+		System.out.println("b ="+b);
+	}
+}
