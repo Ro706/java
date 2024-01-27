cd6d2574a6574386784a96f4493c2d48ba4fdca3
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 16:07:21 +0530

    add file

---
+++ class_work/class2/ExampleChar.java
@@ -0,0 +1,9 @@
+public class  ExampleChar{
+       public static void main(String[] args){
+       		char ch1 = 'a';
+ 		char ch2 = 'A';
+		char ch3 = '1';
+		char ch4 = '@';
+		System.out.println("ch1 = "+ch1+" ch2 = "+ch2+" ch3 = "+ch3+" ch4 = "+ch4);
+		}
+}       
