916415ba370839010a0446f532b992638b68494e
Ro706 <rohitmandal99091@gmail.com>
2024-01-27 15:01:30 +0530

    add file

---
+++ class_work/class2/Exampleint.java
@@ -0,0 +1,6 @@
+public class Exampleint{
+	public static void main(String[] args){
+		int a = 21474385;
+		System.out.println("a= "+a);
+	}
+}
---
+++ class_work/class2/Examplelong.java
@@ -0,0 +1,6 @@
+public class Examplelong{
+	public static void main(String[] args){
+		long a = 9223372036854775803L;
+		System.out.println("a = "+a);
+	}
+}
